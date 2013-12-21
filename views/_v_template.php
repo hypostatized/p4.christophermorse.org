3958160fc04452616bdd3585424eed288e4cd7eb
Christopher Morse <christophermorse@fas.harvard.edu>
2013-12-20 19:03:22 -0500

    profile page functional

--- views/_v_template.php
+++ views/_v_template.php
@@ -2,9 +2,10 @@
 <html>
 <head>
 	<title><?php if(isset($title)) echo $title; ?></title>
-
 	<meta http-equiv="Content-Type" content="text/html; charset=UTF-8" />	
-	<link rel="stylesheet" href="/css/style.css" type="text/javascript">				
+	<link rel="stylesheet" href="/css/style.css" type="text/javascript">
+	<script src="http://code.jquery.com/jquery-latest.min.js" type="text/javascript"></script>
+	<script type="text/javascript" src="/js/raphael.js"></script>				
 	<!-- Controller Specific JS/CSS -->
 	<?php if(isset($client_files_head)) echo $client_files_head; ?>
 	
